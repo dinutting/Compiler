d1f45ce3f6f6478631dd22472d45894dd7dc343c
DragonAkai <dinutting@outlook.com>
2024-07-16 21:28:05 -0400

    fixing name of project

--- Compiler/obj/Debug/net8.0/Lexer.AssemblyInfo.cs
+++ Compiler/obj/Debug/net8.0/Lexer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lexer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15be6cabe3265ae44c3d07e00ff2f1d32364dfe6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lexer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lexer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
